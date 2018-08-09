de58bb297759bfea18892e675eae0890546af9ab
asgtr0623 <asgtr0623>
2018-08-09 12:42:40 +0900

    HW#2

--- week2_20180802/HW2_year_20180809.c
+++ week2_20180802/HW2_year_20180809.c
@@ -1,12 +1,11 @@
-% HW2 : 윤년 계산
 
 #include<stdio.h>
 int main()
 {
 int year;
-prinft("년도를 입력하시오. :");
+printf("년도를 입력하시오. :");
 scanf("%d",&year);
-if(((year%4==0)&&(year%100!=0))\\(year%400==0))
+if(((year%4==0)&&(year%100!=0))||(year%400==0))
 printf("%d 윤년 입니다.\n",year);
 else
 {
